e2eb8c78042e166d6e8cf4ad5073f91ae40ea311
Kristofer Veselits <kveselits@eagles.ewu.edu>
2021-02-10 00:01:58 -0800

    Project skeleton framework using Xamarin.Forms

---
+++ QuestArc/QuestArc/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using QuestArc.Models;
+using QuestArc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QuestArc.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}

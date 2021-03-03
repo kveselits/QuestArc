bee9b2d3e765ea935bfd60e5459f075df4da87cf
Kristofer Veselits <kveselits@eagles.ewu.edu>
2021-03-02 19:09:03 -0800

    initial setup of sqlite (not working) QUEST-13

---
+++ QuestArc/QuestArc.Shared/Models/Quest.cs
@@ -0,0 +1,33 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace QuestArc.Models
+{
+	public enum Difficulty { Easy, Normal, Hard }
+	public class Quest
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+		[Indexed]
+		public String Title { get; set; }
+		public DateTime StartTime { get; set; }
+		public DateTime EndTime { get; set; }
+		public String Description { get; set; }
+		public Difficulty Difficulty { get; set; }
+
+		public Boolean AllDay { get; set; }
+	}
+}
--- QuestArc/QuestArc.Shared/Views/MainPage.xaml.cs
+++ QuestArc/QuestArc.Shared/Views/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using SQLite;
+using QuestArc.Models;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -28,6 +30,42 @@ namespace QuestArc.Views
             InitializeComponent();
             currentPage = typeof(HomePage);
             contentView.Navigate(currentPage);
+            using (var db = TryCreateDatabase())
+            {
+                UpdateList(db);
+            }
+        }
+
+        private static SQLiteConnection TryCreateDatabase()
+        {
+            // Get an absolute path to the database file
+            var databasePath = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "MyData.db");
+
+            var exists = File.Exists(databasePath);
+
+            var db = new SQLiteConnection(databasePath);
+
+            if (!exists)
+            {
+                db.CreateTable<Quest>();
+            }
+
+            return db;
+        }
+
+        private void UpdateList(SQLiteConnection db)
+        {
+            Quests.ItemsSource = db.Table<Quest>().Select(s => s.Title).ToList();
+        }
+
+        public static void AddStock(SQLiteConnection db, string title)
+        {
+            var quest = new Quest()
+            {
+                Title = title
+            };
+            db.Insert(quest);
+            Console.WriteLine("{0} == {1}", quest.Title, quest.Id);
         }
 
         void OnAppBarButtonTapped(object sender, RoutedEventArgs args)

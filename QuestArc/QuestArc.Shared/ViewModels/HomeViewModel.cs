b1dcfe86b2f9aa5e8c70c5cb4eb6533725009ec3
Kristofer Veselits <kveselits@eagles.ewu.edu>
2021-03-08 01:12:33 -0800

    Project refactored to follow MVVM pattern with sample code generated by Windows Template Studio. Merged Home pages into one View.

---
+++ QuestArc/QuestArc.Shared/ViewModels/HomeViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+using QuestArc.Models;
+using QuestArc.Services;
+using WinUI = Microsoft.UI.Xaml.Controls;
+
+namespace QuestArc.ViewModels
+{
+    public class HomeViewModel : ObservableObject
+    {
+        private ICommand _itemInvokedCommand;
+        private object _selectedItem;
+
+        public object SelectedItem
+        {
+            get => _selectedItem;
+            set => SetProperty(ref _selectedItem, value);
+        }
+
+        public ObservableCollection<SampleCompany> SampleItems { get; } = new ObservableCollection<SampleCompany>();
+
+        public ICommand ItemInvokedCommand => _itemInvokedCommand ?? (_itemInvokedCommand = new RelayCommand<WinUI.TreeViewItemInvokedEventArgs>(OnItemInvoked));
+
+        public HomeViewModel()
+        {
+        }
+
+        public async Task LoadDataAsync()
+        {
+            var data = await SampleDataService.GetTreeViewDataAsync();
+            foreach (var item in data)
+            {
+                SampleItems.Add(item);
+            }
+        }
+
+        private void OnItemInvoked(WinUI.TreeViewItemInvokedEventArgs args)
+            => SelectedItem = args.InvokedItem;
+    }
+}
---
+++ QuestArc/QuestArc.Shared/ViewModels/InventoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace QuestArc.ViewModels
+{
+    public class InventoryViewModel : ObservableObject
+    {
+        public InventoryViewModel()
+        {
+        }
+    }
+}

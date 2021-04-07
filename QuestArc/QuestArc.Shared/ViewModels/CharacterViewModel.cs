05e648dd151c06d28bd29930c96695b41a5e140a
Cooperwutzke <36475473+Cooperwutzke@users.noreply.github.com>
2021-04-07 13:55:23 -0700

    Character finally pulls DB Default Char

--- QuestArc/QuestArc.Shared/ViewModels/CharacterViewModel.cs
+++ QuestArc/QuestArc.Shared/ViewModels/CharacterViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-
+using System.Collections.ObjectModel;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using QuestArc.Models;
 
 namespace QuestArc.ViewModels
 {
     public class CharacterViewModel : ObservableObject
     {
+
         public CharacterViewModel()
         {
         }
--- QuestArc/QuestArc.Shared/Views/CharacterPage.xaml.cs
+++ QuestArc/QuestArc.Shared/Views/CharacterPage.xaml.cs
@@ -4,6 +4,7 @@ using QuestArc.ViewModels;
 
 using Windows.UI.Xaml.Controls;
 using QuestArc.Models;
+using Windows.UI.Xaml;
 
 namespace QuestArc.Views
 {
@@ -14,13 +15,104 @@ namespace QuestArc.Views
         public CharacterPage()
         {
             Character character = App.Database.GetCharacterAsync(1).Result;
-            UpdateCharacter(character);
+            this.CharacterRef = character;
+            this._viewName = character.Name;
+            this._viewHealth = character.Health;
+            this._viewStr = character.Strength;
+            this._viewCon = character.Constitution;
+            this._viewDex = character.Dexterity;
+            this._viewWis = character.Wisdom;
+            this._viewCha = character.Charisma;
+            this._viewInt = character.Intelligence;
+            this.numLevel = character.Level;
+            this._viewLevel = "Level: " + Convert.ToString(this.numLevel);
             InitializeComponent();
         }
 
-        private void UpdateCharacter(Character character)
+        public async void StrButton_Pressed(object sender, RoutedEventArgs e)
         {
-            
+            viewStr = viewStr + 1;
+            await App.Database.SaveCharacterAsync(CharacterRef);
+        }
+
+        private async void ConButton_Pressed(object sender, RoutedEventArgs e)
+        {
+            viewCon = viewCon + 1;
+            await App.Database.SaveCharacterAsync(CharacterRef);
+        }
+
+        private async void dexButton_pressed(object sender, RoutedEventArgs e)
+        {
+            viewDex = viewDex + 1;
+            await App.Database.SaveCharacterAsync(CharacterRef);
+        }
+
+
+        private Character CharacterRef;
+
+        private string _viewName;
+        public string viewName 
+        {
+            get => _viewName;
+            set => _viewName = value;
+        }
+
+        private int _viewHealth;
+        public int viewHealth 
+        {
+            get => _viewHealth;
+            set => _viewHealth = value;
+        }
+
+        private int _viewStr;
+        public int viewStr 
+        {
+            get => _viewStr;
+            set => _viewStr = value;
+        }
+
+        private int _viewCon;
+        public int viewCon
+        {
+            get => _viewCon;
+            set => _viewCon = value;
+        }
+
+        private int _viewDex;
+        public int viewDex
+        {
+            get => _viewDex;
+            set => _viewDex = value;
+        }
+
+        private int _viewWis;
+        public int viewWis
+        {
+            get => _viewWis;
+            set => _viewWis = value;
+        }
+
+        private int _viewCha;
+        public int viewCha
+        {
+            get => _viewCha;
+            set => _viewCha = value;
+        }
+
+
+        private int _viewInt;
+        public int viewInt 
+        {
+            get => _viewInt;
+            set => _viewInt = value;
+        }
+
+        private int numLevel;
+        private string _viewLevel;
+        public string viewLevel 
+        {
+            get => _viewLevel;
+            set => _viewLevel = "Level: " + value;
         }
     }
 }

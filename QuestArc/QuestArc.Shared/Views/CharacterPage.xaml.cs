e9b054a2154ae841ae945c6deef11d5143a8c157
Ahall7798 <haustin7798@gmail.com>
2021-05-31 14:12:37 -0700

    Implemented points system

--- QuestArc/QuestArc.Shared/Views/CharacterPage.xaml.cs
+++ QuestArc/QuestArc.Shared/Views/CharacterPage.xaml.cs
@@ -6,12 +6,26 @@ using Windows.UI.Xaml.Controls;
 using QuestArc.Models;
 using Windows.UI.Xaml;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System.Threading.Tasks;
 
 namespace QuestArc.Views
 {
     public sealed partial class CharacterPage : Page
     {
         public CharacterViewModel ViewModel { get; } = new CharacterViewModel();
+        ContentDialogResult result;
+
+        ContentDialog insufficientPointsDialog = new ContentDialog()
+        {
+            Title = "Insufficient points",
+            CloseButtonText = "Ok"
+        };
+
+        ContentDialog areYouSureDialog = new ContentDialog()
+        {
+            CloseButtonText = "Cancel",
+            PrimaryButtonText = "Do It!"
+        };
 
         public CharacterPage()
         {
@@ -20,46 +34,98 @@ namespace QuestArc.Views
             DataContext = ViewModel;
         }
 
+        private async Task displayDialog(int expectedPoints, int actualPoints, string stat)
+        {
+            if(actualPoints < expectedPoints)
+            {
+                int difference = expectedPoints - actualPoints;
+
+                insufficientPointsDialog.Content = "You must acquire " + difference + " more points berfore you can level up " + stat + ".";
+                result = await insufficientPointsDialog.ShowAsync();
+            } else
+            {
+                areYouSureDialog.Content = "Are you sure you want to use " + expectedPoints + " points to level up " + stat + "?";
+                areYouSureDialog.Title = "Level up " + stat + "?";
+                result = await areYouSureDialog.ShowAsync();
+            }
+        }
+
         private async void StrButton_Pressed(object sender, RoutedEventArgs e)
         {
-            ViewModel.viewStr += 1;
-            ViewModel.CharacterRef.Strength += 1;
-            await App.Database.SaveCharacterAsync(ViewModel.CharacterRef);
+            await displayDialog(ViewModel.viewStr * 5, ViewModel.CharacterRef.UnallocatedPoints, "Strength");
+
+            if (result == ContentDialogResult.Primary)
+            {
+                ViewModel.CharacterRef.UnallocatedPoints -= ViewModel.viewStr * 5;
+                ViewModel.viewStr += 1;
+                ViewModel.CharacterRef.Strength += 1;
+                await App.Database.SaveCharacterAsync(ViewModel.CharacterRef);
+            }
         }
 
         private async void ConButton_Pressed(object sender, RoutedEventArgs e)
         {
-            ViewModel.viewCon += 1;
-            ViewModel.CharacterRef.Constitution += 1;
-            await App.Database.SaveCharacterAsync(ViewModel.CharacterRef);
+            await displayDialog(ViewModel.viewCon * 5, ViewModel.CharacterRef.UnallocatedPoints, "Constitution");
+
+            if (result == ContentDialogResult.Primary)
+            {
+                ViewModel.CharacterRef.UnallocatedPoints -= ViewModel.viewCon * 5;
+                ViewModel.viewCon += 1;
+                ViewModel.CharacterRef.Constitution += 1;
+                await App.Database.SaveCharacterAsync(ViewModel.CharacterRef);
+            }
         }
 
         private async void DexButton_Pressed(object sender, RoutedEventArgs e)
         {
-            ViewModel.viewDex += 1;
-            ViewModel.CharacterRef.Dexterity += 1;
-            await App.Database.SaveCharacterAsync(ViewModel.CharacterRef);
+            await displayDialog(ViewModel.viewDex * 5, ViewModel.CharacterRef.UnallocatedPoints, "Dexterity");
+
+            if (result == ContentDialogResult.Primary)
+            {
+                ViewModel.CharacterRef.UnallocatedPoints -= ViewModel.viewDex * 5;
+                ViewModel.viewDex += 1;
+                ViewModel.CharacterRef.Dexterity += 1;
+                await App.Database.SaveCharacterAsync(ViewModel.CharacterRef);
+            }
         }
 
         private async void WisButton_Pressed(object sender, RoutedEventArgs e)
         {
-            ViewModel.viewWis += 1;
-            ViewModel.CharacterRef.Wisdom += 1;
-            await App.Database.SaveCharacterAsync(ViewModel.CharacterRef);
+            await displayDialog(ViewModel.viewWis * 5, ViewModel.CharacterRef.UnallocatedPoints, "Wisdom");
+
+            if (result == ContentDialogResult.Primary)
+            {
+                ViewModel.CharacterRef.UnallocatedPoints -= ViewModel.viewWis * 5;
+                ViewModel.viewWis += 1;
+                ViewModel.CharacterRef.Wisdom += 1;
+                await App.Database.SaveCharacterAsync(ViewModel.CharacterRef);
+            }
         }
 
         private async void ChaButton_Pressed(object sender, RoutedEventArgs e)
         {
-            ViewModel.viewCha += 1;
-            ViewModel.CharacterRef.Charisma += 1;
-            await App.Database.SaveCharacterAsync(ViewModel.CharacterRef);
+            await displayDialog(ViewModel.viewCha * 5, ViewModel.CharacterRef.UnallocatedPoints, "Charisma");
+
+            if (result == ContentDialogResult.Primary)
+            {
+                ViewModel.CharacterRef.UnallocatedPoints -= ViewModel.viewCha * 5;
+                ViewModel.viewCha += 1;
+                ViewModel.CharacterRef.Charisma += 1;
+                await App.Database.SaveCharacterAsync(ViewModel.CharacterRef);
+            }
         }
 
         private async void IntButton_Pressed(object sender, RoutedEventArgs e)
         {
-            ViewModel.viewInt += 1;
-            ViewModel.CharacterRef.Intelligence += 1;
-            await App.Database.SaveCharacterAsync(ViewModel.CharacterRef);
+            await displayDialog(ViewModel.viewInt * 5, ViewModel.CharacterRef.UnallocatedPoints, "Intelligence");
+
+            if (result == ContentDialogResult.Primary)
+            {
+                ViewModel.CharacterRef.UnallocatedPoints -= ViewModel.viewInt * 5;
+                ViewModel.viewInt += 1;
+                ViewModel.CharacterRef.Intelligence += 1;
+                await App.Database.SaveCharacterAsync(ViewModel.CharacterRef);
+            }
         }
     }
 }
